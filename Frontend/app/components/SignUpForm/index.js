b1e2d0c16d1f9d42039862e858867a984faa6bb0
Derya Taşdöğen <derya.tasdogen@gmail.com>
2020-08-10 18:51:34 +0300

    sign up page style added

--- Frontend/app/components/SignUpForm/index.js
+++ Frontend/app/components/SignUpForm/index.js
@@ -10,6 +10,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import fire from '../../config/fire';
+import './style.css';
 
 class SignUpForm extends Component {
   signUp() {
@@ -29,42 +30,54 @@ class SignUpForm extends Component {
 
   render() {
     return (
-      <Container>
-        <Form>
-          <FormGroup row>
-            <Label for="email" sm={12}>
-              E-mail
-            </Label>
-            <Col sm={12} md={{ size: 4, offset: 4 }}>
-              <Input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="E-mail address"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for="password" sm={12}>
-              Password
-            </Label>
-            <Col sm={12} md={{ size: 4, offset: 4 }}>
-              <Input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup>
-            <Link to="/login">Already have an account?</Link>
-          </FormGroup>
-          <Button color="primary" onClick={this.signUp}>
-            SIGN UP
-          </Button>
-        </Form>
-      </Container>
+      <div className="signUp">
+        <Container className="signUpContainer">
+          <Form>
+            <FormGroup>
+              <Col sm={8} md={{ size: 6, offset: 4 }}>
+                <Label className="headerLabel">Sign up to</Label>
+                <Label className="tripify">Tripify</Label>
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="email" sm={2}>
+                E-mail:
+              </Label>
+              <Col sm={8} md={{ size: 6, offset: 1 }}>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="E-mail address"
+                  className="emailInputBox"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="password" sm={2}>
+                Password:
+              </Label>
+              <Col sm={8} md={{ size: 6, offset: 1 }}>
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  className="passwordInputBox"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup>
+              <Link to="/login" className="signUpLink">
+                Already have an account?
+              </Link>
+            </FormGroup>
+            <Button className="signUpButton" onClick={this.signUp}>
+              SIGN UP
+            </Button>
+          </Form>
+        </Container>
+      </div>
     );
   }
 }
